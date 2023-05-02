3e88c82c76ccd1c98126897f01a5003900e1ca33
debarrei <delia.barreiro-bernhardt@capgemini.com>
2023-05-02 17:34:40 +0200

    correccion error en fragments.sql

--- fragments (1).sql
+++ fragments (1).sql
@@ -63,12 +63,10 @@ VALUES
 (4, 3, 1, 'Paco'),
 (5, 3, 2, 'Maria'),
 (6, 3, 3, 'Ilse'),
-(4, 4, 1, 'Lunes, Miercoles'),
-(5, 4, 2, 'Martes, Jueves'),
-(6, 4, 3, 'Una semana lu, mi, otra ma, jue'),
-(6, 4, 4, 'Me da igual'),
-
-(0, 0, 0, '');
+(7, 4, 1, 'Lunes, Miercoles'),
+(8, 4, 2, 'Martes, Jueves'),
+(9, 4, 3, 'Una semana lu, mi, otra ma, jue'),
+(10, 4, 4, 'Me da igual');
 
 
 --INSERT INTO user_topic
