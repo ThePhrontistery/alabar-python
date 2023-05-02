4cf0eed88ef463dd0a30e34246bdcc70b17e269a
debarrei <delia.barreiro-bernhardt@capgemini.com>
2023-05-02 17:01:44 +0200

    modificado fragments.sql sustituido id_owner por owner_name

--- alabar/views.py
+++ alabar/views.py
@@ -1,9 +1,6 @@
 from flask import Blueprint, redirect, render_template, request, session, url_for
 
-from alabar.data import get_topics_by_user
-
-
-from .data import get_topics_by_user
+from alabar.data import get_topics_by_user, get_user_by_id
 
 alabar_bp = Blueprint('alabar', __name__)
 
@@ -14,6 +11,8 @@ def index():
     #pending_surveys = get_pending_surveys_by_user(session['CURRENT_USER'])
     user_id = 1
     table_topics = get_topics_by_user(user_id)
+    for table_topic in table_topics:
+        table_topic.id_owner = get_user_by_id(table_topic.id_owner).name_user
     return render_template('index.html', table_topics=table_topics)
 
 
